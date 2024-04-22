8fe16413386f7a5e514aa6178aa4872e366c1936
root <you@example.com>
2024-04-22 16:00:22 -0700

    Class Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,69 +1,81 @@
 #!/usr/bin/python3
-"""Module Rectangle"""
-
-
+"""Module rectangle."""
+import json
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Class rectangle inheriting attributes of the Base class"""
+    """Class describing a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructor that initializes Rectangle instances"""
-        super().__init__(id)
+        """Initializes a Rectangle instance."""
+
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
-  @property
-  def width(self):
-	  """Retrives width attribute"""
-		 return self.__width
+    @property
+    def width(self):
+        """Retrieves the width attribute."""
 
-		 def height(self):
-            """Retrives height attribute"""
-                  return self.__height
+        return self.__width
 
-		  def x(self):
-           """Retrives x attribute"""
-             return self.__x
+    @property
+    def height(self):
+        """Retrieves the height attribute."""
 
-	def y(self):
-           """Retrives y attribute"""
-                   return self.__y
+        return self.__height
 
-		   @width.setter
-		 def width(self, value):
-			 """sets the width attribute"""
-				 if not isinstance(value, int):
-					 raise TypeError("width must be an integer")
-				if value <= 0:
-				  raise ValueError("width must be > 0")
-				self.__width = value		
-	 	@height.setter
+    @property
+    def x(self):
+        """Retrieves the x attribute."""
 
-	     def height(self, value): 
-		     """sets the height attribute"""
-                        if not isinstance(value, int):  
-				raise TypeError("height must be an integer")
- 	                                 if value <= 0:
-	  raise ValueError("height must be > 0")
+        return self.__x
 
-	    @x.setter
-                   def x(self, value):
-                           """sets the x attribute"""
-                                   if not isinstance(value, int):
-                                           raise TypeError("x must be an integer")
-                                  if value <= 0:
-                                    raise ValueError("x must be > 0")
-                                  self.__x = value
+    @property
+    def y(self):
+        """Retrieves the y attribute."""
 
-		 @y.setter
-		       def y(self, value):
-	 	"""sets the y attribute"""
-                                   if not isinstance(value, int):
-                                           raise TypeError("y must be an integer")
-                                if value <= 0:
-		raise ValueError("y must be > 0")
-                                self.__y = value
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """Sets the width attribute."""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets the height attribute."""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Sets the x attribute."""
+
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Sets the y attribute."""
+
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
