9f1ec96aec84bcb29504d36e73a66926c7462a11
root <you@example.com>
2024-04-22 15:22:09 -0700

    work

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,3 +15,55 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+  @property
+  def width(self):
+	  """Retrives width attribute"""
+		 return self.__width
+
+		 def height(self):
+            """Retrives height attribute"""
+                  return self.__height
+
+		  def x(self):
+           """Retrives x attribute"""
+             return self.__x
+
+	def y(self):
+           """Retrives y attribute"""
+                   return self.__y
+
+		   @width.setter
+		 def width(self, value):
+			 """sets the width attribute"""
+				 if not isinstance(value, int):
+					 raise TypeError("width must be an integer")
+				if value <= 0:
+				  raise ValueError("width must be > 0")
+				self.__width = value		
+	 	@height.setter
+
+	     def height(self, value): 
+		     """sets the height attribute"""
+                        if not isinstance(value, int):  
+				raise TypeError("height must be an integer")
+ 	                                 if value <= 0:
+	  raise ValueError("height must be > 0")
+
+	    @x.setter
+                   def x(self, value):
+                           """sets the x attribute"""
+                                   if not isinstance(value, int):
+                                           raise TypeError("x must be an integer")
+                                  if value <= 0:
+                                    raise ValueError("x must be > 0")
+                                  self.__x = value
+
+		 @y.setter
+		       def y(self, value):
+	 	"""sets the y attribute"""
+                                   if not isinstance(value, int):
+                                           raise TypeError("y must be an integer")
+                                if value <= 0:
+		raise ValueError("y must be > 0")
+                                self.__y = value
