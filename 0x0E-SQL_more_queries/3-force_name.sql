e49104aa7c903837acea090a97d944608fd97348
root <you@example.com>
2024-05-23 12:47:04 -0700

    updated task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,8 +1,3 @@
--- create a database hbtn_0d_2 and the user user_0d_2
--- creates a new database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- create a new user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grant priviledges to this user
-GRANT SELECT ON hbtn_0d_2.*
-TO user_0d_2@localhost;
+-- creates the table force_name on your MySQL server
+-- creates a new table
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
