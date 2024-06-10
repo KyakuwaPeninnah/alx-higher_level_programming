82782d0c0d379c4f86118afc388d89e3a55be9c6
root <you@example.com>
2024-06-10 13:01:01 -0700

    made correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for items in range(0, x):
         try:
-            print("{:d}".format(my_list[items]), end="")
+            print("{}".format(my_list[items]), end="")
             count += 1
-        except:
+        except (IndexError, TypeError):
             pass
 
     print()
