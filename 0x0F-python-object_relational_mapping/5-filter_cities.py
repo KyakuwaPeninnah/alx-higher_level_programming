9dfac40dd0f0747a658d49cd1977ea443f8a1b04
root <you@example.com>
2024-06-22 19:47:48 -0700

    made correction

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""takes in the name of a state as an argument and
+lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 
 if __name__ == '__main__':
 
@@ -10,7 +12,12 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    cur.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
+    db.close()
