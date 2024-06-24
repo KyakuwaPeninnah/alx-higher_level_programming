8f638b03c8cdfd0b9321f3b27f09ad89f860e881
root <you@example.com>
2024-06-24 16:03:19 -0700

    state with a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,20 +7,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db_name = sys.argv[3]
-host = "localhost"
-port = 3306
-engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
-    user, passwd, host, port, db_name))
-Session = sessionmaker(bind=engine)
-local_session = Session()
-state_a = local_session.query(State).filter(
-    State.name.op("regexp")(".*a+.*")).order_by(State.id)
-local_session.close()
-engine.dispose()
-
-if state_a:
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    host = "localhost"
+    port = 3306
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
+        user, passwd, host, port, db_name))
+    Session = sessionmaker(bind=engine)
+    local_session = Session()
+    state_a = local_session.query(State).filter(
+            State.name.op("regexp")(".*a+.*")).order_by(State.id)
+    local_session.close()
+    engine.dispose()
     for state in state_a:
         print(f"{state.id}: {state.name}")
