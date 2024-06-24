6575608ebb499b07959fa801933a2d23fddbbf4f
root <you@example.com>
2024-06-24 15:53:10 -0700

    state with a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""lists all State objects that contain the letter a """
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+db_name = sys.argv[3]
+host = "localhost"
+port = 3306
+engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
+    user, passwd, host, port, db_name))
+Session = sessionmaker(bind=engine)
+local_session = Session()
+state_a = local_session.query(State).filter(
+    State.name.op("regexp")(".*a+.*")).order_by(State.id)
+local_session.close()
+engine.dispose()
+
+if state_a:
+    for state in state_a:
+        print(f"{state.id}: {state.name}")
